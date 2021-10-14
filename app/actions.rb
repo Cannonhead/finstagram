91e8603e1882281df500d6d496ad583639151577
Cannonhead <rseymour5150@gmail.com>
2021-10-14 02:05:50 +0000

    w4d2

--- app/actions.rb
+++ app/actions.rb
@@ -2,3 +2,28 @@ get '/' do
   @finstagram_posts = FinstagramPost.order(created_at: :desc)
   erb(:index)
 end
+
+get '/signup' do
+  @user = User.new  
+  erb(:signup)
+end  
+
+post "/signup" do
+  
+   # grab user input values from params
+  email = params[:email]
+  avatar_url = params[:avatar_url]
+  username = params[:username]
+  password = params[:username]
+
+    # instantiate and save user information
+    @user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password})
+    
+  # checks if user credentials present and saved
+  if @user.save
+    "User #{username} saved!"
+  else
+    erb(:signup) 
+  end
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
 
+    validates :email, :username, uniqueness: true
+    validates :email, :username, :avatar_url, :password, presence: true
+
 end
