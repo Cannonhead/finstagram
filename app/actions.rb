1fdc46eddd1db5d58a9c192852c0d93f81f5e58a
Cannonhead <rseymour5150@gmail.com>
2021-10-19 03:48:31 +0000

    w5d1

--- app/actions.rb
+++ app/actions.rb
@@ -1,3 +1,9 @@
+helpers do
+  def current_user
+    User.find_by(id: session[:user_id])
+  end
+end    
+
 get '/' do
   @finstagram_posts = FinstagramPost.order(created_at: :desc)
   erb(:index)
@@ -5,8 +11,9 @@ end
 
 get '/signup' do
   @user = User.new  
-  erb(:signup)
+  erb(:signup) # render app/views/signup.erb
 end  
+
 
 post "/signup" do
   
@@ -14,7 +21,7 @@ post "/signup" do
   email = params[:email]
   avatar_url = params[:avatar_url]
   username = params[:username]
-  password = params[:username]
+  password = params[:password]
 
     # instantiate and save user information
     @user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password})
@@ -25,5 +32,34 @@ post "/signup" do
   else
     erb(:signup) 
   end
-
 end
+
+get '/login' do
+  erb(:login)     # render app/views/login.erb
+end 
+
+post '/login' do  # when we submit a form with an action of /login
+  username = params[:username]
+  password = params[:password]
+
+  @user = User.find_by(username: username)
+
+    if @user && @user.password == password 
+      session[:user_id] = @user.id   
+      "Success! User #{session[:user_id]} logged in."
+    else
+      @error_message = "Login failed."
+      erb(:login)
+    end
+end
+
+    get '/logout' do
+      session[:user_id] = nil
+      "Logout successful!"
+    end      
+
+    get '/' do
+      @finstagram_posts = FInstagrampost.order(created_at: :desc)
+      @current_user = User.find.by(id: session[:user_id])
+      erb(:index)
+    end  
